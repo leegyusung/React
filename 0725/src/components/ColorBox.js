32dcbbca683c9ae8a0dcafc6b9be9bbb242e34ac
leegyusung <lgs7542@naver.com>
2021-07-25 17:30:41 +0900

    3

---
+++ 0725/src/components/ColorBox.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import ColorContext, { ColorConsumer } from '../contexts/color';
+
+const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+    return (
+        <ColorConsumer>
+            {value => (
+                <>
+                    <div style={{
+                        width: '64px',
+                        height: '64px',
+                        background: state.color
+                    }}>
+                    </div>
+                    <div style={{
+                        width: '32px',
+                        height: '32px',
+                        background: state.subcolor
+                    }}>
+                    </div>
+                </>
+            )}
+        </ColorConsumer>
+    );
+};
+
+export default ColorBox;
